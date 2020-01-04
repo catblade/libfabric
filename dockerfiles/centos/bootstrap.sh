45d39c1e2d4341df427606e5cf6df946541dbef8
catblade <catblade@catblade.tech>
2020-01-04 04:26:46 +0000

    remove unused pieces.

--- dockerfiles/centos/bootstrap.sh
+++ dockerfiles/centos/bootstrap.sh
@@ -5,5 +5,8 @@ bash autogen.sh
 ./configure
 make
 make install
+rm -rf /libfabric
+yum remove -y gcc make
+yum clean all
 ldconfig
 fi_info
--- dockerfiles/ubuntu/bootstrap.sh
+++ dockerfiles/ubuntu/bootstrap.sh
@@ -5,5 +5,14 @@ bash autogen.sh
 ./configure
 make
 make install
+rm -rf /libfabric
+apt-get remove -y dh-autoreconf
+apt-get clean -y
+rm -rf \
+   /var/cache/debconf/* \
+   /var/lib/apt/lists/* \
+   /var/log/* \
+   /tmp/* \
+   /var/tmp/*
 ldconfig
 fi_info
