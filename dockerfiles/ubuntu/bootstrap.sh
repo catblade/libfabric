2d8b836a886296bdbf3a8f7b309c18f8e02e5eeb
catblade <catblade@catblade.tech>
2020-01-11 15:57:59 +0000

    update ubuntu libfabric image.  Now only slightly larger than ubuntu regular size.

--- dockerfiles/ubuntu/bootstrap.sh
+++ dockerfiles/ubuntu/bootstrap.sh
@@ -5,14 +5,9 @@ bash autogen.sh
 ./configure
 make
 make install
-rm -rf /libfabric
-apt-get remove -y dh-autoreconf
-apt-get clean -y
-rm -rf \
-   /var/cache/debconf/* \
-   /var/lib/apt/lists/* \
-   /var/log/* \
-   /tmp/* \
-   /var/tmp/*
+mkdir /libfabric/libfabric_libs
+cp -P /libfabric/src/.libs/libfabric.* /libfabric/libfabric_libs/.
 ldconfig
-fi_info
+
+
+
